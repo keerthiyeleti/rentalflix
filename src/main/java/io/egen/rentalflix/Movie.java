06efca92a9773ab6dc7604cc13ce6e494db6c773
keerthiyeleti <keerthi_yeleti@yahoo.com>
2016-02-18 02:21:46 -0600

    Feat(Movie) Added getters and setters

--- src/main/java/io/egen/rentalflix/Movie.java
+++ src/main/java/io/egen/rentalflix/Movie.java
@@ -5,5 +5,37 @@ package io.egen.rentalflix;
  * Fields: id, title, year, language
  */
 public class Movie {
-	//POJO IMPLEMENTATION GOES HERE
+	
+private String id;
+private String title;
+private int year;
+private String language;
+
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public int getYear() {
+	return year;
+}
+public void setYear(int year) {
+	this.year = year;
+}
+public String getLanguage() {
+	return language;
+}
+public void setLanguage(String language) {
+	this.language = language;
+}
+
+	
+	
 }
